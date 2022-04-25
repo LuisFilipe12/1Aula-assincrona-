642e60e3747aede49d973f9942c0ee0a8e32b73b
LuisFilipe12 <luis.f.sampaio12@gmail.com>
2022-04-25 16:20:21 -0300

    faz parte da primeira aula assíncrona

--- Hello.java
+++ Hello.java
@@ -8,6 +8,24 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		Scanner leia = new Scanner(System.in);
+		
+
+		double media; 
+		double bimestre1;
+		double bimestre2;
+		double bimestre3;
+		double bimestre4;
+		System.out.print("Entre com a primeira nota do aluno: ");
+		bimestre1 = leia.nextDouble();
+		System.out.println("Entre com a segunda nota: ");
+		bimestre2 = leia.nextDouble();
+		System.out.println("Entre com a terceira nota: ");
+		bimestre3 = leia.nextDouble();
+		System.out.println("Entre com a quarta nota: ");
+		bimestre4 = leia.nextDouble();
+		media = (bimestre1 + bimestre2 + bimestre3 + bimestre4) /4;
+		
+		System.out.println("A media do aluno foi: " + media);
 
 		System.out.println("Hello World");
 
