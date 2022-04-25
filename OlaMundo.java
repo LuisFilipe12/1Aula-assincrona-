7d07a09f05919fe91970da0da8809db807b4d4ce
LuisFilipe12 <luis.f.sampaio12@gmail.com>
2022-04-25 16:07:02 -0300

    faz parte da primeira aula assincrona

---
+++ OlaMundo.java
@@ -0,0 +1,30 @@
+package aulas;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+    
+	System.out.println("Hello World!");
+	
+	int idade = 12; 
+	double real = 6.12;
+	String apelido = "Luís Filipe";
+	char sexo = 'M';
+	
+	
+	System.out.println(apelido + " tem " + idade + " anos e possui R$ " + real);
+	
+    
+	
+	
+	
+		
+
+	
+	
+	
+	
+	
+	}
+
+}
